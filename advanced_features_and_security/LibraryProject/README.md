41d341dee5893dac022621b3eecff13001c940cf
Haywhice <lalaayomideisaac@gmail.com>
2025-03-09 06:18:27 +0100

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -1,1 +1,19 @@
 Introduction to Django Development Environment Setup
+
+HOW PERMISSIONS AND GROUPS ARE CONFIGURED AND USED
+
+Permissions:
+
+    can_view: Allows viewing posts.
+    can_create: Allows creating posts.
+    can_edit: Allows editing posts.
+    can_delete: Allows deleting posts.
+Groups:
+
+    Editors: Can create and edit posts.
+    Viewers: Can view posts.
+    Admins: Can perform all actions.
+
+Views:
+
+    Views are protected using Django's @permission_required decorator.
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
+from .models import Book
+from .forms import BookForm
+from .forms import BookSearchForm 
+from .forms import ExampleForm 
+from django.views.decorators.csrf import csrf_protect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.decorators
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
 class LibraryDetailView(DetailView):
@@ -29,4 +34,14 @@ class LibrarianView(TemplateView):
 class MemberView(TemplateView):
     template_name = "relationship_app/Member_view.html"
     
-@method_decorator(permission_required())
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/book_form.html', {'form': form})
